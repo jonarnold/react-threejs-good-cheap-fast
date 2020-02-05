804c7c5a9178292b00281e9eaaa843d25f3e3e68
jon@tuitivegroup.com <jon@tuitivegroup.com>
2020-02-05 09:28:44 -0500

    blender uvs

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import ThreeTest from './components/ThreeTest';
+
+function App() {
+  return (
+    <div className="App">
+      <ThreeTest />
+    </div>
+  );
+}
+
+export default App;
