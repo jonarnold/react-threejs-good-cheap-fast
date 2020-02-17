ad9a59ed2c2553f3bc257c5e70c1e1e26739f570
jon@tuitivegroup.com <jon@tuitivegroup.com>
2020-02-17 15:03:21 -0500

    animating child mesh! :)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import ThreeTest from './components/ThreeTest';
+import GoodFastCheap from './components/GoodFastCheap';
 
 function App() {
   return (
     <div className="App">
-      <ThreeTest />
+      <GoodFastCheap />
     </div>
   );
 }
